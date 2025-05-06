8d0972301eadd168e86a8c33c5ed4f74a6c74ec9
Şükriye Öztürk <sukriyeo13@gmail.com>
2025-05-06 22:19:35 +0300

    feat(admin) backend system for admin and customerDTO added, more to come

--- code/src/main/java/pentacode/backend/code/admin/repository/AdminRepository.java
+++ code/src/main/java/pentacode/backend/code/admin/repository/AdminRepository.java
@@ -1,5 +1,6 @@
-package pentacode.backend.code.admin;
+package pentacode.backend.code.admin.repository;
 
+import pentacode.backend.code.admin.entity.Admin;
 import pentacode.backend.code.common.repository.base.BaseRepository;
 
 public interface  AdminRepository extends BaseRepository<Admin> {
